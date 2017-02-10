1bd4d03d6caa8e04ef9eb20bb8b5214c5d31c8ae
yuzelli <516440912@qq.com>
2017-02-10 14:46:03 +0800

    u

--- app/src/main/java/com/example/buiderdream/apehire/view/fragment/UserMineFragment.java
+++ app/src/main/java/com/example/buiderdream/apehire/view/fragment/UserMineFragment.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.buiderdream.apehire.R;
+import com.example.buiderdream.apehire.base.BaseFragment;
 
 /**
  * Created by Administrator on 2016/12/4.
  */
 
-public class UserMineFragment extends Fragment {
+public class UserMineFragment extends BaseFragment {
     private View userMineFragmentView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
@@ -44,6 +45,7 @@ public class UserMineFragment extends Fragment {
     public void onDestroy() {
         ViewGroup parent = (ViewGroup) userMineFragmentView.getParent();
         if (parent!=null){
+            //移除当前视图，防止重复加载相同视图使得程序闪退
             parent.removeView(userMineFragmentView);
         }
         super.onDestroy();
