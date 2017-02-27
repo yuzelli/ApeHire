6547d4a570438fa3cdf7031964aee3a607e92311
yuzelli <516440912@qq.com>
2017-02-27 21:03:47 +0800

    u

---
+++ app/src/main/java/com/example/buiderdream/apehire/bean/JobAndCompany.java
@@ -0,0 +1,180 @@
+package com.example.buiderdream.apehire.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by 51644 on 2017/2/27.
+ */
+
+public class JobAndCompany implements Serializable {
+
+    /**
+     * companyAddress : 北京上地中关村
+     * companyHeadImg : http://ojterpx44.bkt.clouddn.com/13133443000_1488105970188
+     * companyId : 7
+     * companyIntroduce : 阿里巴巴网络技术有限公司（简称：阿里巴巴集团）是以曾担任英语教师的马云为首的18人，于1999年在杭州创立，他们相信互联网能够创造公平的竞争环境，让小企业通过创新与科技扩展业务，并在参与国内或全球市场竞争时处于更有利的位置。[1] 阿里巴巴集团经营多项业务，另外也从关联公司的业务和服务中取得经营商业生态系统上的支援。业务和关联公司的业务包括：淘宝网、天猫、聚划算、全球速卖通、阿里巴巴国际交易市场、1688、阿里妈妈、阿里云、蚂蚁金服、菜鸟网络等。[2]  2014年9月19日，阿里巴巴集团在纽约证券交易所正式挂牌上市，股票代码“BABA”，创始人和董事局主席为马云。2015年全年，阿里巴巴总营收943.84亿元人民币，净利润688.44亿元人民币。2016年4月6日，阿里巴巴正式宣布已经成为全球最大的零售交易平台。
+     * companyName : 阿里巴巴
+     * companyNum : 13133443000
+     * companyPassword : 123
+     * companyScale : 3
+     */
+
+    private CompanyBean company;
+    /**
+     * company : {"companyAddress":"北京上地中关村","companyHeadImg":"http://ojterpx44.bkt.clouddn.com/13133443000_1488105970188","companyId":7,"companyIntroduce":"阿里巴巴网络技术有限公司（简称：阿里巴巴集团）是以曾担任英语教师的马云为首的18人，于1999年在杭州创立，他们相信互联网能够创造公平的竞争环境，让小企业通过创新与科技扩展业务，并在参与国内或全球市场竞争时处于更有利的位置。[1] 阿里巴巴集团经营多项业务，另外也从关联公司的业务和服务中取得经营商业生态系统上的支援。业务和关联公司的业务包括：淘宝网、天猫、聚划算、全球速卖通、阿里巴巴国际交易市场、1688、阿里妈妈、阿里云、蚂蚁金服、菜鸟网络等。[2]  2014年9月19日，阿里巴巴集团在纽约证券交易所正式挂牌上市，股票代码\u201cBABA\u201d，创始人和董事局主席为马云。2015年全年，阿里巴巴总营收943.84亿元人民币，净利润688.44亿元人民币。2016年4月6日，阿里巴巴正式宣布已经成为全球最大的零售交易平台。","companyName":"阿里巴巴","companyNum":"13133443000","companyPassword":"123","companyScale":3}
+     * jobCharge : 1
+     * jobCity : 1
+     * jobDetail : ccccccccc
+     * jobId : 4
+     * jobName : android
+     * jobTechnology : cccccccccccccccccc
+     * jobType : 1
+     */
+
+    private int jobCharge;
+    private int jobCity;
+    private String jobDetail;
+    private int jobId;
+    private String jobName;
+    private String jobTechnology;
+    private int jobType;
+
+    public CompanyBean getCompany() {
+        return company;
+    }
+
+    public void setCompany(CompanyBean company) {
+        this.company = company;
+    }
+
+    public int getJobCharge() {
+        return jobCharge;
+    }
+
+    public void setJobCharge(int jobCharge) {
+        this.jobCharge = jobCharge;
+    }
+
+    public int getJobCity() {
+        return jobCity;
+    }
+
+    public void setJobCity(int jobCity) {
+        this.jobCity = jobCity;
+    }
+
+    public String getJobDetail() {
+        return jobDetail;
+    }
+
+    public void setJobDetail(String jobDetail) {
+        this.jobDetail = jobDetail;
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getJobTechnology() {
+        return jobTechnology;
+    }
+
+    public void setJobTechnology(String jobTechnology) {
+        this.jobTechnology = jobTechnology;
+    }
+
+    public int getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(int jobType) {
+        this.jobType = jobType;
+    }
+
+    public static class CompanyBean {
+        private String companyAddress;
+        private String companyHeadImg;
+        private int companyId;
+        private String companyIntroduce;
+        private String companyName;
+        private String companyNum;
+        private String companyPassword;
+        private int companyScale;
+
+        public String getCompanyAddress() {
+            return companyAddress;
+        }
+
+        public void setCompanyAddress(String companyAddress) {
+            this.companyAddress = companyAddress;
+        }
+
+        public String getCompanyHeadImg() {
+            return companyHeadImg;
+        }
+
+        public void setCompanyHeadImg(String companyHeadImg) {
+            this.companyHeadImg = companyHeadImg;
+        }
+
+        public int getCompanyId() {
+            return companyId;
+        }
+
+        public void setCompanyId(int companyId) {
+            this.companyId = companyId;
+        }
+
+        public String getCompanyIntroduce() {
+            return companyIntroduce;
+        }
+
+        public void setCompanyIntroduce(String companyIntroduce) {
+            this.companyIntroduce = companyIntroduce;
+        }
+
+        public String getCompanyName() {
+            return companyName;
+        }
+
+        public void setCompanyName(String companyName) {
+            this.companyName = companyName;
+        }
+
+        public String getCompanyNum() {
+            return companyNum;
+        }
+
+        public void setCompanyNum(String companyNum) {
+            this.companyNum = companyNum;
+        }
+
+        public String getCompanyPassword() {
+            return companyPassword;
+        }
+
+        public void setCompanyPassword(String companyPassword) {
+            this.companyPassword = companyPassword;
+        }
+
+        public int getCompanyScale() {
+            return companyScale;
+        }
+
+        public void setCompanyScale(int companyScale) {
+            this.companyScale = companyScale;
+        }
+    }
+}
--- app/src/main/java/com/example/buiderdream/apehire/view/fragment/CollectionHireFragment.java
+++ app/src/main/java/com/example/buiderdream/apehire/view/fragment/CollectionHireFragment.java
@@ -1,13 +1,24 @@
 package com.example.buiderdream.apehire.view.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.buiderdream.apehire.R;
 import com.example.buiderdream.apehire.base.BaseFragment;
+import com.example.buiderdream.apehire.bean.CompanyInfo;
+import com.example.buiderdream.apehire.bean.JobAndCompany;
+import com.example.buiderdream.apehire.bean.UserInfo;
+import com.example.buiderdream.apehire.utils.CommonAdapter;
+import com.example.buiderdream.apehire.utils.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by binglong_li on 2017/2/12.
@@ -16,6 +27,11 @@ import com.example.buiderdream.apehire.base.BaseFragment;
 
 public class CollectionHireFragment extends BaseFragment{
     private View collectionHireFragmentView;
+    private ListView lv_hire;
+    private List<JobAndCompany> jobList;
+    private CommonAdapter<JobAndCompany> adapter;
+    private Context context;
+    private UserInfo userInfo;
 
     @Nullable
     @Override
@@ -32,10 +48,31 @@ public class CollectionHireFragment extends BaseFragment{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        jobList = new ArrayList<>();
         initView();
+
     }
 
     private void initView() {
+        lv_hire = (ListView) collectionHireFragmentView.findViewById(R.id.lv_hire);
+    }
+    /**
+     * 更新listView
+     */
+    private void updataListView() {
+        final List<String> citys = new ArrayList<>(Arrays.asList("不限", "北京", "上海", "广州", "深圳", "武汉", "杭州", "成都", "西安"));
+        final List<String> jobCharges = new ArrayList<>(Arrays.asList("不限", "3k-5k", "5k-10k", "10k-15k", "15k-20k", "20k-30k", "30k-50k"));
+        adapter = new CommonAdapter<JobAndCompany>(context, jobList, R.layout.fragment_hire_item) {
+            @Override
+            public void convert(ViewHolder helper, JobAndCompany item) {
+                helper.setImageByUrl2(R.id.job_item_img, item.getCompany().getCompanyHeadImg());
+                helper.setText(R.id.job_item_jobName, item.getJobName());
+                helper.setText(R.id.job_item_companyAddress, citys.get(item.getJobCity()));
+                helper.setText(R.id.job_item_companyName, item.getCompany().getCompanyName());
+                helper.setText(R.id.job_item_jobCharge, jobCharges.get(item.getJobCharge()));
+            }
+        };
+        lv_hire.setAdapter(adapter);
     }
 
     @Override
